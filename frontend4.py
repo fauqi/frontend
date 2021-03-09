08a26be8f3bfdebcf800eb962d8bc38e66b04441
fauqi <fa.asyrafi@gmail.com>
2021-03-09 16:19:42 +0700

    stock dan start+pause aman goto popup enter

--- frontend4.py
+++ frontend4.py
@@ -42,8 +42,8 @@ def appendCad():
             labelUser[j].place(x=0.0285*b.sW,y=j*((0.078*b.sH)+0.09*b.sH)+(0.243*b.sH),width=0.056*b.sW,height=0.078*b.sH,anchor=W)
 
 
-        pnjangBtn=0.843 * b.sW
-        
+        wContainer=0.843 * b.sW
+        k=26
         btnTask =[0 for x in range(88)]
         gambar=[0 for y in range(60)]
         for i in range(k):
@@ -53,59 +53,67 @@ def appendCad():
             
             #btnTask[i].config(command=lambda:b.tertekan(result[i]))
             
-            if kontainer == 1:
-                if k<=maksJob:
-                    btnTask[i].place(x=i*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.175*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH) 
-                else :
-                    btnTask[i].place(x=i*(((pnjangBtn-((k+1)*0.01*b.sW))/k)+0.01*b.sW)+((0.163*b.sW)),y=0.175*b.sH,width=(pnjangBtn-((0.01*b.sW)+((k+1)*0.01*b.sW)))/k,height=0.158*b.sH) 
-            elif  kontainer ==2 :
-                if i<=6 :
-                    btnTask[i].place(x=i*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.175*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH) 
-                elif i>6:
-                    c=i-7
-                    #print(c)
-                    btnTask[i].place(x=c*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.347*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH) 
-            elif kontainer == 3:
-                if i<=6 :
-                    btnTask[i].place(x=i*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.175*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH) 
-                elif i>6 and i <=13:
-                    c=i-7
-                   #
-                   # print(c)
-                    btnTask[i].place(x=c*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.347*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH)
-                elif i>13:
-                    d=i-14
-                    btnTask[i].place(x=d*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.52*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH)
-            elif kontainer == 4:
-                if i<=6 :
-                    btnTask[i].place(x=i*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.175*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH) 
-                elif i>6 and i <=13:
-                    c=i-7
-                    #print(c)
-                    btnTask[i].place(x=c*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.347*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH)
-                elif i>13 and i<=20:
-                    d=i-14
-                    btnTask[i].place(x=d*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.52*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH)
-                elif i>20:
-                    e=i-21
-                    btnTask[i].place(x=e*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.7*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH)
+            # if kontainer == 1:
+            # if k<=maksJob:
+            #     btnTask[i].place(x=i*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.175*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH) 
+            # else :
+            #     btnTask[i].place(x=i*(((pnjangBtn-((k+1)*0.01*b.sW))/k)+0.01*b.sW)+((0.163*b.sW)),y=0.175*b.sH,width=(pnjangBtn-((0.01*b.sW)+((k+1)*0.01*b.sW)))/k,height=0.158*b.sH) 
+            # elif  kontainer ==2 :
+            #     if i<=6 :
+            #         btnTask[i].place(x=i*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.175*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH) 
+            #     elif i>6:
+            #         c=i-7
+            #         #print(c)
+            #         btnTask[i].place(x=c*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.347*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH) 
+            # elif kontainer == 3:
+            #     if i<=6 :
+            #         btnTask[i].place(x=i*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.175*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH) 
+            #     elif i>6 and i <=13:
+            #         c=i-7
+            #        #
+            #        # print(c)
+            #         btnTask[i].place(x=c*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.347*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH)
+            #     elif i>13:
+            #         d=i-14
+            #         btnTask[i].place(x=d*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.52*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH)
+            # elif kontainer == 4:
+            # itembaris=k/kontainer
+            # if itembaris<maksJob:
+
+
+            # if i<=6 :
+            print((int(i/7)*0.190*b.sH)+(0.175*b.sH))
+            panjangButton=(wContainer-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7
+            offsetH=0.170*b.sH
+            offsetW=0.163*b.sW
+            btnTask[i].place(x=(i%7)* (panjangButton+(0.01*b.sW))+offsetW,y=((int(i/7)*0.190*b.sH)+offsetH),width=panjangButton,height=0.158*b.sH) 
+            # elif i>6 and i <=13:
+            #     c=i-7
+            #     #print(c)
+            #     btnTask[i].place(x=c*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.347*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH)
+            # elif i>13 and i<=20:
+            #     d=i-14
+            #     btnTask[i].place(x=d*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.52*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH)
+            # elif i>20:
+            #     e=i-21
+            #     btnTask[i].place(x=e*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.7*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH)
 
 appendCad()
 
-for i in range(k):
-    x=i
-    try:
-        gambar[x]=loadImageWebPublic(result[x]['image'],0,80)
-        btnTask[i].config(image=gambar[x])
-       # print("ada gambar")
-    except:
-        print("no Image")
-        photo=Image.open("no image.png")
-        photo =photo.resize((80, 80), Image.ANTIALIAS)
-        gambar[x]= ImageTk.PhotoImage(photo)
-        btnTask[i].config(image=gambar[x])
-    #print("luar try")
-    btnTask[i].config(command=lambda x=i,id=result[x]['id']:b.tertekan(result[x],id))
+# for i in range(k):
+#     x=i
+#     try:
+#         gambar[x]=loadImageWebPublic(result[x]['image'],0,80)
+#         btnTask[i].config(image=gambar[x])
+#        # print("ada gambar")
+#     except:
+#         print("no Image")
+#         photo=Image.open("no image.png")
+#         photo =photo.resize((80, 80), Image.ANTIALIAS)
+#         gambar[x]= ImageTk.PhotoImage(photo)
+#         btnTask[i].config(image=gambar[x])
+#     #print("luar try")
+#     btnTask[i].config(command=lambda x=i,id=result[x]['id']:b.tertekan(result[x],id))
     
 
 #print(result[0]['is_stock'])
