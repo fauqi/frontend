d7652e08b3ed6c01ad3b86b7123f3e7d46b4bed8
fauqi <fa.asyrafi@gmail.com>
2021-03-09 18:02:26 +0700

    tinggal nunggu api per karyawan

--- frontend4.py
+++ frontend4.py
@@ -10,83 +10,86 @@ from Beranda import *
 
 b=Beranda(lontong)
 b.showLayar()
+jumlahJob=0
 
-
-# def increment():
-#     global k
+def increment():
+    global jumlahJob
     
-#    # print(k)
-#     for i in range(k):
-#         btnTask[i].destroy()
-#     k=k+1
-#     appendCad()
+   # print(k)
+    for i in range(jumlahJob):
+        btnTask[i].destroy()
+    jumlahJob=jumlahJob+1
+    appendCad(1,3)
 
-def appendCad():
+def listKaryawan():
+    tryButton = Button(b.frame,text= "mbak bi",command = increment)
+    tryButton.place(x=200,y=50,width=50,height=50)
+    
+    labelUser=len(b.pegawai)
+
+    labelPegawai=[0 for z in range(5)]
+    labelUser=[0 for y in range(5)]
+    for j in range(len(b.pegawai)):
+        labelPegawai[j]= Label(b.frame,text = b.pegawai[j],bg="WHITE",fg='#1687A7',font=10)
+        labelPegawai[j].place(x=0.085*b.sW,y=j*((0.078*b.sH)+0.09*b.sH)+(0.243*b.sH),width=0.056*b.sW,height=0.078*b.sH,anchor=W)
+        labelUser[j]= Label(b.frame,text="Lontong",image = b.userGambar,bg="WHITE")
+        labelUser[j].place(x=0.0285*b.sW,y=j*((0.078*b.sH)+0.09*b.sH)+(0.243*b.sH),width=0.056*b.sW,height=0.078*b.sH,anchor=W)
+
+
+def appendCad(bariskaryawan,kontainer=1):
+        global jumlahJob
         global  btnTask,k,result,gambar
         maksJob=7
-        kontainer=1
         #print(len(result))
         k=len(result)
-       
-        # tryButton = Button(b.frame,text= "mbak bi",command = increment)
-        # tryButton.place(x=200,y=50,width=50,height=50)
-       
-        labelUser=len(b.pegawai)
-
-        labelPegawai=[0 for z in range(5)]
-        labelUser=[0 for y in range(5)]
-        for j in range(len(b.pegawai)):
-            labelPegawai[j]= Label(b.frame,text = b.pegawai[j],bg="WHITE",fg='#1687A7',font=10)
-            labelPegawai[j].place(x=0.085*b.sW,y=j*((0.078*b.sH)+0.09*b.sH)+(0.243*b.sH),width=0.056*b.sW,height=0.078*b.sH,anchor=W)
-            labelUser[j]= Label(b.frame,text="Lontong",image = b.userGambar,bg="WHITE")
-            labelUser[j].place(x=0.0285*b.sW,y=j*((0.078*b.sH)+0.09*b.sH)+(0.243*b.sH),width=0.056*b.sW,height=0.078*b.sH,anchor=W)
-
-
+  
         wContainer=0.843 * b.sW
-        k=26
         btnTask =[0 for x in range(88)]
         gambar=[0 for y in range(60)]
-        for i in range(k):
-            btnTask[i] = Button(b.frame,text="hallo "+str(i))
+        containerJob=[0 for y in range(kontainer)]
+        itembaris=jumlahJob/kontainer
+        if itembaris<maksJob:
+            k=7
+            kelebihan=0
+        else:
+            k=int(jumlahJob/kontainer)
+            kelebihan= jumlahJob%kontainer
+        lastBaris=0
 
+        for i in range(kontainer):
+            if kelebihan>0:
+                containerJob[i]=k+1
+                kelebihan=kelebihan-1
+            else :
+                containerJob[i]=k
+            print("jumlah container"+str(i)+"="+str(containerJob[i]))
+        jumlahItem=containerJob[0]
+        icontainer=0
+        x=0
 
+        print("mulai \r\n")
+        for i in range(jumlahJob):
+            btnTask[i] = Button(b.frame,text="hallo "+str(i))            
+            btnTask[i].config(command=lambda:b.tertekan(result[i]))
             
-            #btnTask[i].config(command=lambda:b.tertekan(result[i]))
-            
-            # if kontainer == 1:
-            # if k<=maksJob:
-            #     btnTask[i].place(x=i*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.175*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH) 
-            # else :
-            #     btnTask[i].place(x=i*(((pnjangBtn-((k+1)*0.01*b.sW))/k)+0.01*b.sW)+((0.163*b.sW)),y=0.175*b.sH,width=(pnjangBtn-((0.01*b.sW)+((k+1)*0.01*b.sW)))/k,height=0.158*b.sH) 
-            # elif  kontainer ==2 :
-            #     if i<=6 :
-            #         btnTask[i].place(x=i*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.175*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH) 
-            #     elif i>6:
-            #         c=i-7
-            #         #print(c)
-            #         btnTask[i].place(x=c*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.347*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH) 
-            # elif kontainer == 3:
-            #     if i<=6 :
-            #         btnTask[i].place(x=i*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.175*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH) 
-            #     elif i>6 and i <=13:
-            #         c=i-7
-            #        #
-            #        # print(c)
-            #         btnTask[i].place(x=c*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.347*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH)
-            #     elif i>13:
-            #         d=i-14
-            #         btnTask[i].place(x=d*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.52*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH)
-            # elif kontainer == 4:
-            # itembaris=k/kontainer
-            # if itembaris<maksJob:
+            if(x>=(containerJob[icontainer]-1)):
+                icontainer=icontainer+1
+            sigmaContainerTerlewat=0
+            for m in range(icontainer):
+                sigmaContainerTerlewat=sigmaContainerTerlewat+containerJob[m]
+
+            x=i-sigmaContainerTerlewat
+          
+            panjangButton=(wContainer-((0.01*b.sW)+((jumlahItem+1)*0.01*b.sW)))/containerJob[icontainer]
+        
+            offsetH=0.170*b.sH + (bariskaryawan*0.180*b.sH)
+            offsetW=0.163*b.sW            
+            btnTask[i].place(x=(x)*(panjangButton+(0.01*b.sW))+offsetW,y=((int(icontainer)*0.180*b.sH)+offsetH),width=panjangButton,height=0.158*b.sH) 
+            print("i="+str(i) +",x="+str(x)+",sigma="+str(sigmaContainerTerlewat) +",icontainer= " + str(icontainer))
+
+          
 
 
-            # if i<=6 :
-            print((int(i/7)*0.190*b.sH)+(0.175*b.sH))
-            panjangButton=(wContainer-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7
-            offsetH=0.170*b.sH
-            offsetW=0.163*b.sW
-            btnTask[i].place(x=(i%7)* (panjangButton+(0.01*b.sW))+offsetW,y=((int(i/7)*0.190*b.sH)+offsetH),width=panjangButton,height=0.158*b.sH) 
             # elif i>6 and i <=13:
             #     c=i-7
             #     #print(c)
@@ -98,9 +101,8 @@ def appendCad():
             #     e=i-21
             #     btnTask[i].place(x=e*(((pnjangBtn-((7+1)*0.01*b.sW))/7)+0.01*b.sW)+((0.163*b.sW)),y=0.7*b.sH,width=(pnjangBtn-((0.01*b.sW)+((7+1)*0.01*b.sW)))/7,height=0.158*b.sH)
 
-appendCad()
 
-# for i in range(k):
+# for i in range(len(result)):
 #     x=i
 #     try:
 #         gambar[x]=loadImageWebPublic(result[x]['image'],0,80)
@@ -119,4 +121,5 @@ appendCad()
 #print(result[0]['is_stock'])
 
 #btnTask[0].config(image=gambar10)
+listKaryawan()
 lontong.mainloop()
