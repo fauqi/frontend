0e7fb0cab79bb1cbb0031a6ff894df259dc4331f
fauqi <fa.asyrafi@gmail.com>
2021-03-18 11:57:22 +0700

    tinggal thread

--- coba_threading.py
+++ coba_threading.py
@@ -1,19 +1,22 @@
 import threading
 print("bisa kok")
 import time
-
+import queue
+flagThread=0
 def delay5():
     event.set()
     print("mulai 5 detik")  
     time.sleep(5)
     print("selesai")
     event.clear()
+
 def penyela():  
-    event.wait()
-    if event.is_start == True:
+    #event.wait()
+    if event.is_set():
         print("tersela ni ges yak")
 
 event = threading.Event()
 t1= threading.Thread(target=delay5)
 t2= threading.Thread(target=penyela)
-delay5()
+t1.start()
+t2.start()
